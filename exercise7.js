2bda2c0c5015bc81ef9ec7a6e1447c6b2196ea7e
Dan <git@dandart.co.uk>
2014-03-23 19:48:47 +0000

    Exercise 7 working

--- exercise7.js
+++ exercise7.js
@@ -10,17 +10,54 @@
      * @author Dan Dart
      * @return bool
     **/
-    Btree.Tree.prototype.hasPathSum = function() {
-        throw {name: 'NotImplementedError', message: 'Not Implemented'};
+    Btree.Tree.prototype.hasPathSum = function(intSum) {
+        var _this = this,
+            intRemainingSum = intSum - _this.getValue();
+
+        if (0 === intRemainingSum) {
+            return true;
+        }
+
+        if (_this.hasLeft() &&
+            _this.getLeft().hasPathSum(intRemainingSum)) {
+            return true;
+        }
+
+        if (_this.hasRight() &&
+            _this.getRight().hasPathSum(intRemainingSum)) {
+            return true;
+        }
+
+        return false;
     };
 
-    var node = new Btree.Node(4),
+    var node = new Btree.Node(5),
         tree = new Btree.Tree(node);
 
+    /**
+     * Seeing as the tree given in the question
+     * was actually invalid (or at least unsorted)
+     * let's generate one like it that works...
+     *
+     *       5
+     *     4     8
+     *   2     7  11
+     * 1             13
+     *
+     *   It has 3 sums, 12, 20, 37. These should return true.
+    **/
+    tree.insert(4);
+    tree.insert(8);
     tree.insert(2);
-    tree.insert(5);
+    tree.insert(7);
+    tree.insert(11);
     tree.insert(1);
-    tree.insert(3);
+    tree.insert(13);
 
-    console.log(tree.hasPathSum(Number(process.argv[2])));
+
+    if ('undefined' == typeof process.argv[2]) {
+        console.log('Please provide a path sum. UsageL '+process.argv[1] + ' (sum)');
+        return;
+    }
+    console.log(tree.hasPathSum(Number(process.argv[2])).toString());
 })();
